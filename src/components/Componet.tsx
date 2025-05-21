da366ab49d3bae7e8d166adc8408fd39ef1285c2
Aman-Mateen-Zahoor <amman.w4u@gmail.com>
2025-05-21 13:32:33 +0500

    HI 2

---
+++ src/components/Componet.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Componet = () => {
+  return (
+    <View>
+      <Text>Componet</Text>
+    </View>
+  )
+}
+
+export default Componet
+
+const styles = StyleSheet.create({})
